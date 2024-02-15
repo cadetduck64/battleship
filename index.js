65b453bd5cee16489116cd5f95fc99c53d4aa518
Gil <joyce.gilbert64@gmail.com>
2024-02-15 00:13:13 -0500

    added logic to prevent duplicate ship placements

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-
-
 const ship = (shipName, length, hitCount, sunkStatus, coordinates) => {
     const newShip = {
         shipName,
@@ -49,10 +47,12 @@ const isSunk = (ship) => {
 // const destroyer = gameboard.placeShip(4, 'destroyer', 4, 'b')
 
 const gameboard = () => {
+    const shipCoordinateList = []
     const xAxis = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     const yAxis = ['a','b','c','d','e','f','g','h','i','j']
     const missed = []
     const receiveAttack = (ship, attempt) => {
+        console.log(shipCoordinateList)
         if (missed.toString().includes(attempt.toString()))
         {return console.log('invalid option; target has been hit already')}
         // console.log((attempt))
@@ -63,43 +63,65 @@ const gameboard = () => {
     }
     
     const placeShip = (length, shipName, xAxisChoice, yAxisChoice, orientation) => {
-        shipName = ship(shipName, length, 0, false, [[xAxisChoice, yAxisChoice]])
-        if (orientation === 'vertical')
-            {
+        shipName = ship(shipName, length, 0, false, [], orientation)
+
+        
+        if (orientation === 'vertical')     {
                 const yOrigin = yAxis.indexOf(yAxisChoice)
-                let shipYCoordinateArr = (yAxis.slice(yOrigin, yOrigin + ship.length - 1))
-                console.log(shipYCoordinateArr)
+                let shipYCoordinateArr = (yAxis.slice(yOrigin, yOrigin + ship.length - 2))
+
                 for (const element of shipYCoordinateArr) {
+                    // if (shipCoordinateList.includes([xAxisChoice, element]))
+                    //     {return console.log('invalid placement')}
+                    
+                    for (let i = 0; i < shipCoordinateList.length; i++)
+                    {let choice = [xAxisChoice, element]
+                        if(shipCoordinateList[i] == choice.toString())
+                    {return console.log('invalid option')}}
+
                     shipName.coordinates.push([xAxisChoice, element])
+                    shipCoordinateList.push([xAxisChoice, element])
                 }
+
             } else if (orientation === 'horizontal')    {
                 const xOrigin = xAxis.indexOf(xAxisChoice)
-                let shipXCoordinateArr = (xAxis.slice(xOrigin, xOrigin + ship.length - 1))
-                console.log(shipXCoordinateArr)
+                let shipXCoordinateArr = (xAxis.slice(xOrigin, xOrigin + ship.length - 2))
                 for (const element of shipXCoordinateArr) {
+                    
+                    for (let i = 0; i < shipCoordinateList.length; i++)
+                    {let choice = [element, yAxisChoice]
+                        if(shipCoordinateList[i] == choice.toString())
+                    {return console.log('invalid option')}}
+                    
                     shipName.coordinates.push([element, yAxisChoice])
+                    shipCoordinateList.push([element, yAxisChoice])
                 }
-            }
-        
-        
-            
+            }      
+            console.log(shipCoordinateList)
         return shipName
     }
-
+    const gameOverCheck = () => {
+        if ('list' == shipCoordinateList)
+        {return 'Game Over'}
+    }
+    gameOverCheck()
 return {xAxis, yAxis, missed, receiveAttack, placeShip}
 }
 
 const player1Gameboard = gameboard()
 
-const cruiser = player1Gameboard.placeShip(3, 'cruiser', 3, 'a', 'horizontal')
+const cruiser = player1Gameboard.placeShip(3, 'cruiser', 3, 'a', 'vertical')
+const destroyer = player1Gameboard.placeShip(3, 'destroyer', 3, 'a', 'horizontal')
 
-player1Gameboard.receiveAttack(cruiser, [6, 'a'])
-player1Gameboard.receiveAttack(cruiser, [6, 'a'])
+console.log(cruiser)
+console.log(destroyer)
+
+
+// player1Gameboard.receiveAttack(cruiser, [6, 'a'])
+// player1Gameboard.receiveAttack(cruiser, [6, 'a'])
 // player1Gameboard.receiveAttack(cruiser, [5, 'a'])
 // player1Gameboard.receiveAttack(cruiser, [4, 'a'])
 // player1Gameboard.receiveAttack(cruiser, [3, 'a'])
-
-console.log(cruiser)
 
 console.log('asdf')
 
@@ -109,3 +131,5 @@ console.log('asdf')
 
 // export {player1Gameboard}
 // export {cruiser}
+
+//added logic to prevent duplicate ship placements
