f87e2a12c8284cc77a3ffbd0e66ff49d6bff0625
Gil <joyce.gilbert64@gmail.com>
2024-04-01 23:50:31 -0400

    created an interactive user interface

---
+++ DOMScript.js
@@ -0,0 +1,192 @@
+import {player1Gameboard} from "/index.js"
+import {ships} from "/index.js"
+
+console.log(ships)
+
+const playFieldContainer = document.querySelector('#playFieldContainer')
+const playField = document.querySelector('#playField')
+const xAxisDiv = document.querySelector('#xAxisDiv')
+const yAxisDiv = document.querySelector('#yAxisDiv')
+const shipListDiv = document.querySelector('#shipListDiv')
+const placeShipModal = document.querySelector('#placeShipModal')
+let mouseHoverVariable
+let mouseClickVariable
+let DOMorientation = 'horizontal'
+let currentShipLength
+let currentShipName
+// placeShipModal.style.visbility = false
+
+const gameboardDom = () => {
+    const DOMxAxis = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    const DOMyAxis = ['a','b','c','d','e','f','g','h','i','j']
+
+    const renderxAxis = () => {
+        for (let i = 0; i < 10; i ++)
+        {const xAxisSquare = document.createElement('div')
+        xAxisSquare.classList.add('xAxisSquare')
+        xAxisSquare.textContent = DOMxAxis[i]
+        xAxisDiv.appendChild(xAxisSquare)
+        playFieldContainer.appendChild(xAxisDiv)
+        }
+    }
+
+    const renderyAxis = () => {
+        for (let i = 0; i < 10; i ++)
+        {const yAxisSquare = document.createElement('div')
+        yAxisSquare.classList.add('yAxisSquare')
+        yAxisSquare.textContent = DOMyAxis[i]
+        yAxisDiv.appendChild(yAxisSquare)
+        playFieldContainer.appendChild(yAxisDiv)
+        }
+    }
+
+
+    const renderPlayfield = () => {
+        const previewEngine = (previewEngineLength) => {
+                // const previewEngineSquare = document.getElementById(mouseClickVariable+previewEngineLength)
+                if(DOMorientation === 'horizontal')
+                {   // console.log((Number(mouseHoverVariable.id) + previewEngineLength - 1))
+                    for(let i = 1; i < previewEngineLength; i++)
+                    {document.getElementById(Number(mouseHoverVariable.id) + previewEngineLength - i).classList.add('battlefieldSquareHighlight')}
+                    
+                } else if(DOMorientation === 'vertical')
+                    for(let i = 1; i < previewEngineLength ; i++)
+                    if (document.getElementById(Number(mouseHoverVariable.id) + 10 * i) === null) {return} else
+                    {document.getElementById(Number(mouseHoverVariable.id) + 10 * i).classList.add('battlefieldSquareHighlight')}
+            }
+
+            const clearSquares = () => {
+                let a = document.querySelectorAll('.battlefieldSquareHighlight')
+                let b = document.querySelectorAll('.battlefieldSquareSelect')
+                b.forEach(element => {
+                    element.classList.remove('battlefieldSquareSelect')
+                });
+
+                a.forEach(element => {
+                    element.classList.remove('battlefieldSquareHighlight')
+                });
+            }
+
+            for (let i = 0; i < 100; i ++)
+            {
+                const battlefieldSquare = document.createElement('div')
+                battlefieldSquare.classList.add(`battlefieldSquare`)
+                battlefieldSquare.setAttribute('id', i)
+                
+                battlefieldSquare.addEventListener('mouseover', () => {
+                    mouseHoverVariable = battlefieldSquare, battlefieldSquare.classList.add('battlefieldSquareSelect'),
+                    previewEngine(currentShipLength)})
+                    
+                battlefieldSquare.addEventListener('mouseleave', () => {
+                    clearSquares()
+                })
+                
+                // battlefieldSquare.addEventListener('mouseover', () => {console.log(Number(mousePoint.id))})
+                battlefieldSquare.addEventListener('click', () => {
+                    if(currentShipName === undefined)
+                    {return console.log('Pick a ship first, Commander')}
+                    
+
+                    const DOMconflictCheck = document.getElementsByClassName('deployedShip')
+                    console.log(DOMconflictCheck)
+                    // array.forEach(element => {
+                        
+                    // });
+
+                    console.log(Number(mouseHoverVariable.id))
+                    mouseClickVariable = Number(mouseHoverVariable.id)
+
+                    // cannot place ships after Id 80
+                    console.log(DOMxAxis[mouseHoverVariable.id[1]], DOMyAxis[mouseHoverVariable.id[0]])
+                    
+                    if (mouseHoverVariable.id[1] === undefined) {
+                        console.log(DOMxAxis[mouseHoverVariable.id[0]], DOMyAxis[0])
+                        player1Gameboard.placeShip(currentShipLength, currentShipName, DOMxAxis[mouseHoverVariable.id[0]], DOMyAxis[0], DOMorientation)
+                    } else {
+                        player1Gameboard.placeShip(currentShipLength, currentShipName, DOMxAxis[mouseHoverVariable.id[1]], DOMyAxis[mouseHoverVariable.id[0]], DOMorientation)
+                    }
+
+                    if(DOMorientation === 'horizontal')
+                    {  
+                        for(let i = 1; i < currentShipLength; i++)
+                        document.getElementById(Number(mouseHoverVariable.id) + currentShipLength - i).classList.add('deployedShip')
+                        mouseHoverVariable.classList.add('deployedShip')
+                    } else if (DOMorientation === 'vertical') { 
+                        for(let i = 1; i < currentShipLength ; i++)
+                        document.getElementById(Number(mouseHoverVariable.id) + 10 * i).classList.add('deployedShip')
+                        mouseHoverVariable.classList.add('deployedShip')
+                    }
+
+                    console.log(player1Gameboard.activeShipList)
+                })
+
+                battlefieldSquare.addEventListener('contextmenu', (e) => {e.preventDefault()
+                    if(DOMorientation === 'horizontal')
+                    {  
+                        clearSquares()
+                        DOMorientation = 'vertical'
+                        previewEngine(currentShipLength)
+                    } else if (DOMorientation === 'vertical') { 
+                        clearSquares()  
+                        DOMorientation = 'horizontal'
+                        previewEngine(currentShipLength)}
+                })
+
+            battlefieldSquare.textContent = i
+            playField.appendChild(battlefieldSquare)
+            playFieldContainer.appendChild(playField)
+        }
+    }
+
+
+    const placeShipDom = () => {
+
+        // player1Gameboard.placeShip
+
+        const renderShipSummaries =() => {
+            const shipSummaryFactory = (length, shipName) => {
+
+                const shipSummary = document.createElement('div')
+                shipSummary.textContent = shipName + ' || length: ' + length
+                shipSummary.classList.add('shipSummary')
+
+                // shipSummary.addEventListener('contextmenu', (e) => {e.preventDefault(), console.log('right')})
+
+                shipSummary.addEventListener('click', () => {
+                    currentShipLength = length
+                    currentShipName = shipName
+                    console.log('currentShipLength ' +currentShipLength)
+                    console.log('currentShipName '+currentShipName)
+
+                    const removehightlight = document.getElementById('shipSummaryHighlight')
+                    if(removehightlight != undefined || removehightlight != null)
+                    {removehightlight.removeAttribute('id')}
+                    shipSummary.setAttribute('id', 'shipSummaryHighlight')
+                })
+                shipListDiv.appendChild(shipSummary)
+            }
+            
+            ships.forEach(element => {
+                shipSummaryFactory(element.shipObjectLength, element.shipObjectName)
+            });
+            // const cruiserSummary = shipSummaryFactory(3, 'cruiser')
+            // const battleshipSummary = shipSummaryFactory(4, 'battleship')
+            // const carrier = shipSummaryFactory(5, 'carrier')
+
+        }
+        renderShipSummaries()
+        console.log(player1Gameboard.activeShipList)
+
+}
+
+    placeShipDom()
+    renderxAxis()
+    renderyAxis()
+    renderPlayfield()
+}
+gameboardDom()
+
+//algorithm for finding coordinates: using the index of Y and X to find the corresponding class for the grid
+//  Example: if you want to hit g, 7
+// y[6] = G; x[6] = 7
+// this would correspond to the 67th tile
--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const ship = (shipName, length, hitCount, sunkStatus, coordinates) => {
         coordinates,
         hit: () => {
             newShip.hitCount++
-        },
+        }
     }
     return newShip
 }
@@ -19,42 +19,25 @@ const isSunk = (ship) => {
     {return console.log(ship.shipName + ' is Still intact')}
 }
 
-// const gameboardFunc = () => {
-
-//     gameboard = {
-//         xAxis: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-//         yAxis: ['a','b','c','d','e','f','g','h','i','j'],
-//         missed: [],
-//         receiveAttack: () => {(ship, attempt) => {
-//             console.log(attempt, ship.coordinates)
-//             if (attempt.toString() === ship.coordinates.toString())
-//             {return 'hit'}
-//             }
-//         },
-//         placeShip: (length, shipName, xAxis, yAxis) => {
-//             shipName = ship(length, 0, false, [xAxis, yAxis])
-//             console.log(shipName)
-//             return shipName
-//         }
-//     }
-//     console.log(gameboard)
-//     return  gameboard
-// }
-// const cruiser = gameboard.placeShip(3, 'cruiser', 3,'a')
-// console.log(gameboard.receiveAttack())
-// console.log(cruiser)
-
-// const destroyer = gameboard.placeShip(4, 'destroyer', 4, 'b')
-
 const gameboard = () => {
     const shipCoordinateList = []
     const xAxis = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     const yAxis = ['a','b','c','d','e','f','g','h','i','j']
     const missed = []
+    const activeShipList = []
+    const gameOverCheck = () => {
+        activeShipList.forEach(element => {
+            if (element.sunkStatus == true)
+            {let newSunkShip = activeShipList.indexOf(element)
+            activeShipList.splice(newSunkShip, 1)
+        } else if (activeShipList.length == 0)
+        {return 'Game Over, You Lose'}
+        })
+    }
+
     const receiveAttack = (ship, attempt) => {
-        console.log(shipCoordinateList)
         if (missed.toString().includes(attempt.toString()))
-        {return console.log('invalid option; target has been hit already')}
+        {return 'invalid option; target has been hit already'}
         // console.log((attempt))
         // console.log((ship.coordinates))
         else if (ship.coordinates.toString().includes(attempt.toString()))
@@ -64,76 +47,114 @@ const gameboard = () => {
     
     const placeShip = (length, shipName, xAxisChoice, yAxisChoice, orientation) => {
         shipName = ship(shipName, length, 0, false, [], orientation)
+                        
+        // console.log(shipName.length)
+        console.log(yAxisChoice)
 
-        
-        if (orientation === 'vertical')     {
+        //checks if ship clips is within the grid
+
+
+
+
+        if (orientation === 'vertical') {
                 const yOrigin = yAxis.indexOf(yAxisChoice)
-                let shipYCoordinateArr = (yAxis.slice(yOrigin, yOrigin + ship.length - 2))
+                let shipYCoordinateArr = (yAxis.slice(yOrigin, yOrigin + shipName.length))
 
-                if (ship.length + yOrigin > 12)
-                {return console.log('invalid option, ship out of bounds')}
+                //checks if ship is out of bounds
+                if (shipName.length + yOrigin > 10)
+                {return console.log(`invalid option ship is out of bounds, place the ${shipName.shipName} somewhere else`)}
 
+                // if  (shipName.length + yAxisChoice > 10)
+                // {return console.log('out of grid')}
+
+                //check if coordinates are occupied by another ship
                 for (const element of shipYCoordinateArr) {                    
                     for (let i = 0; i < shipCoordinateList.length; i++)
                     {let choice = [xAxisChoice, element]
                         if(shipCoordinateList[i] == choice.toString())
-                    {return console.log('invalid option theres already a ship there')}}
+                    {return console.log(`invalid option theres already a ship there, place the ${shipName.shipName} somewhere else`)}}
 
                     shipName.coordinates.push([xAxisChoice, element])
                     shipCoordinateList.push([xAxisChoice, element])
                 }
-
+                activeShipList.push(shipName)
             } else if (orientation === 'horizontal')    {
                 const xOrigin = xAxis.indexOf(xAxisChoice)
-                let shipXCoordinateArr = (xAxis.slice(xOrigin, xOrigin + ship.length - 2))
+                let shipXCoordinateArr = (xAxis.slice(xOrigin, xOrigin + shipName.length))
 
-                if (ship.length + xOrigin > 12)
-                {return console.log('invalid option, ship out of bounds')}
+                //check if ship is out of bounds
+                if (shipName.length + xAxisChoice > 11) {
+                    return console.log(`invalid option ship is out of bounds, place the ${shipName.shipName} somewhere else`)}
 
-                for (const element of shipXCoordinateArr) {
-                    
+                if (shipName.length + xOrigin > 12)
+                {return console.log(`invalid option ship is out of bounds, place the ${shipName.shipName} somewhere else`)}
+
+                
+                //check if coordinates are occupied by another ship
+                for (const element of shipXCoordinateArr) {                   
                     for (let i = 0; i < shipCoordinateList.length; i++)
                     {let choice = [element, yAxisChoice]
                         if(shipCoordinateList[i] == choice.toString())
-                    {return console.log('invalid option theres already a ship there')}}
+                    {return console.log(`invalid option theres already a ship there, place the ${shipName.shipName} somewhere else`)}}
                     
                     shipName.coordinates.push([element, yAxisChoice])
-                    shipCoordinateList.push([element, yAxisChoice])
+                    shipCoordinateList.push([element, yAxisChoice])                   
                 }
+                activeShipList.push(shipName)
             }      
-            console.log(shipCoordinateList)
+            // console.log(shipCoordinateList)
         return shipName
     }
-    const gameOverCheck = () => {
-        if ('list' == shipCoordinateList)
-        {return 'Game Over'}
-    }
-    gameOverCheck()
-return {xAxis, yAxis, missed, receiveAttack, placeShip}
+
+return {xAxis, yAxis, missed, receiveAttack, placeShip, activeShipList, gameOverCheck}
 }
 
+const cpuGameboard = gameboard()
 const player1Gameboard = gameboard()
 
-const cruiser = player1Gameboard.placeShip(3, 'cruiser', 1, 'i', 'vertical')
-const destroyer = player1Gameboard.placeShip(3, 'destroyer', 9, 'c', 'horizontal')
+// const carrier = player1Gameboard.placeShip(5, 'carrier')
+// const battleship = player1Gameboard.placeShip(4, 'battleship')
+// const cruiser = player1Gameboard.placeShip(3, 'cruiser', 3, 'a', 'vertical')
+// const submarine = player1Gameboard.placeShip(3,'submarine', 5, 'd', 'vertical')
+// const destroyer = player1Gameboard.placeShip(2, 'destroyer', 6, 'c', 'horizontal')
+// const ssConflict = player1Gameboard.placeShip(3, 'ssConflict', 3, 'a', 'vertical')
 
-console.log(cruiser)
-console.log(destroyer)
+const carrier = {shipObjectLength: 5, shipObjectName: 'carrier'}
+const battleship = {shipObjectLength: 4, shipObjectName: 'battleship'}
+const cruiser = {shipObjectLength: 3, shipObjectName: 'cruiser'}
+const submarine = {shipObjectLength: 3, shipObjectName: 'submarine'}
+const destroyer = {shipObjectLength: 2, shipObjectName: 'destroyer'}
+const ssConflict = (3, 'ssConflict')
 
+console.log(carrier)
+
+const ships = [
+    battleship,
+    carrier,
+    cruiser,
+    destroyer,
+    submarine
+]
+
+
+const referee = () => {
+
+    
+    player1Gameboard.gameOverCheck()
+    return player1Gameboard, cpuGameboard
+}
+
+referee()
 
 // player1Gameboard.receiveAttack(cruiser, [6, 'a'])
 // player1Gameboard.receiveAttack(cruiser, [6, 'a'])
-// player1Gameboard.receiveAttack(cruiser, [5, 'a'])
-// player1Gameboard.receiveAttack(cruiser, [4, 'a'])
+// player1Gameboard.receiveAttack(cruiser, [3, 'c'])
+// player1Gameboard.receiveAttack(cruiser, [3, 'b'])
 // player1Gameboard.receiveAttack(cruiser, [3, 'a'])
 
-console.log('asdf')
-
-
-//take the x and y coordinates, use that to determine ship position, write tests
-// make player objects
-
 // export {player1Gameboard}
+// export {boat}
 // export {cruiser}
 
-//added logic to prevent duplicate ship placements
+export {player1Gameboard}
+export {ships}
