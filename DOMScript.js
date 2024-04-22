39fab94ef58bbac141af2016ba34d740fba01abc
Gil <joyce.gilbert64@gmail.com>
2024-04-22 18:58:53 -0400

    fixed hit bot detection and DOM hit updates

--- DOMScript.js
+++ DOMScript.js
@@ -1,8 +1,12 @@
 import {player1Gameboard} from "/index.js"
+import {botGameboard } from "/index.js"
 import {ships} from "/index.js"
+import { referee } from "/index.js"
 
 console.log(ships)
 
+
+//player used variables
 const playFieldContainer = document.querySelector('#playFieldContainer')
 const playField = document.querySelector('#playField')
 const xAxisDiv = document.querySelector('#xAxisDiv')
@@ -15,11 +19,20 @@ let DOMorientation = 'horizontal'
 let currentShipLength
 let currentShipName
 let DOMCoordinates
-// placeShipModal.style.visbility = false
 
-const gameboardDom = () => {
-    const DOMxAxis = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    const DOMyAxis = ['a','b','c','d','e','f','g','h','i','j']
+//Bot used variables
+const botContainer = document.querySelector('#botContainer')
+const botplayfieldcontainer = document.querySelector('#botplayfieldcontainer')
+const botplayField = document.querySelector('#botplayField')
+const botxAxisDiv = document.querySelector('#botxAxisDiv')
+const botyAxisDiv = document.querySelector('#botyAxisDiv')
+let botmouseHoverVariable
+
+//easy DOM reference for index code coordinates
+const DOMxAxis = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const DOMyAxis = ['a','b','c','d','e','f','g','h','i','j']
+//player 1 functions
+const gameboardPlayer1Dom = () => {
 
     const renderxAxis = () => {
         for (let i = 0; i < 10; i ++)
@@ -84,11 +97,14 @@ const gameboardDom = () => {
                 
                 // battlefieldSquare.addEventListener('mouseover', () => {console.log(Number(mousePoint.id))})
                 battlefieldSquare.addEventListener('click', () => {
-                    if(currentShipName === undefined)
+                    // console.log(player1Gameboard.activeShipList.length === ships.length)
+                    if(player1Gameboard.activeShipList.length === ships.length)
+                    {return console.log('you cant fire on your own ships')}
+                    else if(currentShipName === undefined)
                     {return console.log('Pick a ship first, Commander')}
 
-                    console.log(Number(mouseHoverVariable.id))
-                    mouseClickVariable = Number(mouseHoverVariable.id)
+                    // console.log(Number(mouseHoverVariable.id))
+                    // mouseClickVariable = Number(mouseHoverVariable.id)
 
                     //checks valid placement
                     if (mouseHoverVariable.id[1] === undefined)
@@ -143,11 +159,7 @@ const gameboardDom = () => {
         }
     }
 
-
     const placeShipDom = () => {
-
-        // player1Gameboard.placeShip
-
         const renderShipSummaries =() => {
             const shipSummaryFactory = (length, shipName) => {
 
@@ -190,9 +202,154 @@ const gameboardDom = () => {
     renderyAxis()
     renderPlayfield()
 }
-gameboardDom()
+
+gameboardPlayer1Dom()
+
+//renders CPU/player 2 gameboard
+
+const DOMbotGameboard = () => {
+
+    const botDOMxAxis = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    const botDOMyAxis = ['a','b','c','d','e','f','g','h','i','j']
+
+    const botrenderxAxis = () => {
+        for (let i = 0; i < 10; i ++)
+        {const botxAxisSquare = document.createElement('div')
+        botxAxisSquare.classList.add('botxAxisSquare')
+        botxAxisSquare.textContent = botDOMxAxis[i]
+        botxAxisDiv.appendChild(botxAxisSquare)
+        // botplayfieldcontainer.appendChild(botxAxisDiv)
+        }
+    }
+
+    const botrenderyAxis = () => {
+        for (let i = 0; i < 10; i ++)
+        {const botyAxisSquare = document.createElement('div')
+        botyAxisSquare.classList.add('botyAxisSquare')
+        botyAxisSquare.textContent = botDOMyAxis[i]
+        botyAxisDiv.appendChild(botyAxisSquare)
+        // botplayfieldcontainer.appendChild(botyAxisDiv)
+        }
+    }
+
+
+    const botrenderplayField = () => {
+        const previewEngine = (previewEngineLength) => {
+            // const previewEngineSquare = document.getElementById(mouseClickVariable+previewEngineLength)
+            if(DOMorientation === 'horizontal')
+            {   // console.log((Number(mouseHoverVariable.id) + previewEngineLength - 1))
+                for(let i = 1; i < previewEngineLength; i++)
+                {document.getElementById(Number(mouseHoverVariable.id) + previewEngineLength - i).classList.add('battlefieldSquareHighlight')}
+                
+            } else if(DOMorientation === 'vertical')
+                for(let i = 1; i < previewEngineLength ; i++)
+                if (document.getElementById(Number(mouseHoverVariable.id) + 10 * i) === null) {return} else
+                {document.getElementById(Number(mouseHoverVariable.id) + 10 * i).classList.add('battlefieldSquareHighlight')}
+        }
+
+
+        const botclearSquares = () => {
+            let a = document.querySelectorAll('.botbattlefieldSquareHighlight')
+            let b = document.querySelectorAll('.botbattlefieldSquareSelect')
+            b.forEach(element => {
+                element.classList.remove('botbattlefieldSquareSelect')
+            });
+
+            a.forEach(element => {
+                element.classList.remove('botbattlefieldSquareHighlight')
+            });
+        }
+
+            for (let i = 0; i < 100; i ++)  {
+                const botbattlefieldSquare = document.createElement('div')
+                botbattlefieldSquare.classList.add(`botbattlefieldSquare`)
+                botbattlefieldSquare.setAttribute('id', i)
+                
+                botbattlefieldSquare.addEventListener('mouseover', () => {
+                    mouseHoverVariable = botbattlefieldSquare, botbattlefieldSquare.classList.add('botbattlefieldSquareSelect'),
+                    previewEngine(currentShipLength)})
+                
+                botbattlefieldSquare.addEventListener('mouseleave', () => {
+                    let a = document.querySelectorAll('.battlefieldSquareHighlight')
+                    let b = document.querySelectorAll('.battlefieldSquareSelect')
+                    b.forEach(element => {
+                        element.classList.remove('battlefieldSquareSelect')
+                    });
+    
+                    a.forEach(element => {
+                        element.classList.remove('battlefieldSquareHighlight')
+                    });
+                    botclearSquares()
+                })
+                
+                botbattlefieldSquare.addEventListener('click', () => {           
+                    // if (player1Gameboard.activeShipList.length < ships.length)
+                    // {return console.log('Place all ships before launching attacks, Commander')}
+
+                    (Number(mouseHoverVariable.id[1])) 
+
+                    let botHitAttempt = [botDOMxAxis[mouseHoverVariable.id[1]], botDOMyAxis[mouseHoverVariable.id[0]]]
+                    if (botHitAttempt[0] === undefined) {botHitAttempt = [botDOMxAxis[mouseHoverVariable.id[0]], 'a']}
+                               
+                    //function that finds coordinates corresponding to ship hit
+                    for (let i = 0; i < botGameboard.activeShipList.length; i++)    
+                    {
+                        let shipCheck = botGameboard.activeShipList[i]
+                        for (let i = 0; i < shipCheck.coordinates.length; i++)
+                        {   if (shipCheck.coordinates[i].toString().includes(botHitAttempt, 0)) 
+                            {
+                            mouseHoverVariable.classList.add('botbattlefieldSquareHitShip')
+                            return (botGameboard.receiveAttack(shipCheck, botHitAttempt), botGameboard.gameOverCheck())
+                            } 
+                            // else {return botGameboard.receiveAttack(shipCheck, botHitAttempt), mouseHoverVariable.classList.add('botbattlefieldSquareMissedShip')}
+                        }
+                    }
+                    botGameboard.receiveAttack(undefined, botHitAttempt)
+                    botGameboard.gameOverCheck()
+                    
+                    if (mouseHoverVariable.classList.contains('.botbattlefieldSquareHitShip') === false)
+                    {mouseHoverVariable.classList.add('botbattlefieldSquareMissedShip')}
+                    
+                    
+                })
+
+                botbattlefieldSquare.addEventListener('contextmenu', (e) => {e.preventDefault()
+                })
+
+                botbattlefieldSquare.textContent = i
+                botplayField.appendChild(botbattlefieldSquare)
+
+                
+            }
+
+
+            }
+            botrenderxAxis()
+            botrenderyAxis()
+            botrenderplayField()
+        
+    }
+    
+    DOMbotGameboard()
+    
+    // console.log(botGameboard)
+    // console.log((botGameboard.activeShipList[0], [2, 'a']))
+
+    // //function that finds coordinates corresponding to ship hit
+    // for (let i = 0; i < botGameboard.activeShipList.length; i++)    
+    // {
+    //     let shipCheck = botGameboard.activeShipList[i]
+    //     for (let i = 0; i < shipCheck.coordinates.length; i++)
+    //     {if (shipCheck.coordinates[i].includes(2, 'a')) {console.log(shipCheck.shipName)}}
+    // }
+
+
 
 //algorithm for finding coordinates: using the index of Y and X to find the corresponding class for the grid
 //  Example: if you want to hit g, 7
 // y[6] = G; x[6] = 7
 // this would correspond to the 67th tile
+
+// shipCheck.coordinates.forEach(element => {
+//     if (element.includes(2, 'a')) {return console.log(shipCheck.shipName)}
+// })
--- index.js
+++ index.js
@@ -1,125 +1,177 @@
-const messageBoard = document.getElementById('messageBoard')
+const messageBoard = document.getElementById("messageBoard");
 
 const ship = (shipName, length, hitCount, sunkStatus, coordinates) => {
-    const newShip = {
-        shipName,
-        length,
-        hitCount,
-        sunkStatus,
-        coordinates,
-        hit: () => {
-            newShip.hitCount++
-        }
-    }
-    return newShip
-}
+  const newShip = {
+    shipName,
+    length,
+    hitCount,
+    sunkStatus,
+    coordinates,
+    hit: () => {
+      newShip.hitCount++;
+    },
+  };
+  return newShip;
+};
 
 const isSunk = (ship) => {
-    if (ship.hitCount >= ship.length)
-    {ship.sunkStatus = true
-    return console.log(ship.shipName + ' is SUNK')} else
-    {return console.log(ship.shipName + ' is Still intact')}
-}
+  if (ship.hitCount >= ship.length) {
+    ship.sunkStatus = true;
+    return console.log(ship.shipName + " is SUNK");
+  } else {
+    return console.log(ship.shipName + " is Still intact");
+  }
+};
 
 const gameboard = () => {
-    const shipCoordinateList = []
-    const xAxis = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    const yAxis = ['a','b','c','d','e','f','g','h','i','j']
-    const missed = []
-    const activeShipList = []
-    const gameOverCheck = () => {
-        activeShipList.forEach(element => {
-            if (element.sunkStatus == true)
-            {let newSunkShip = activeShipList.indexOf(element)
-            activeShipList.splice(newSunkShip, 1)
-        } else if (activeShipList.length == 0)
-        {return 'Game Over, You Lose'}
-        })
-    }
+  const shipCoordinateList = [];
+  const xAxis = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const yAxis = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"];
+  const missed = [];
+  const activeShipList = [];
+  const gameOverCheck = () => {
+    console.log('GAMEOVER CHECK')
+    console.log(botGameboard.activeShipList)
+    console.log(activeShipList.length)
 
-    const receiveAttack = (ship, attempt) => {
-        if (missed.toString().includes(attempt.toString()))
-        {return 'invalid option; target has been hit already'}
-        // console.log((attempt))
-        // console.log((ship.coordinates))
-        else if (ship.coordinates.toString().includes(attempt.toString()))
-        {return ship.hitCount++, missed.push(attempt), isSunk(ship), 'hit'} else 
-        {return missed.push(attempt), isSunk(ship), 'miss'}
-    }
+    if (activeShipList.length === 0) {
+        return console.log("Game Over, You Lose");
+      }
+    activeShipList.forEach((element) => {
+
+        if (element.sunkStatus === true) {
+        let newSunkShip = activeShipList.indexOf(element);
+        activeShipList.splice(newSunkShip, 1);
+      }
+    });
+  };
+
+//   const receiveAttack = (ship, attempt) => {
+//     console.log(ship)
+//     console.log(attempt)
+//     if (missed.toString().includes(attempt.toString())) {
+//       return "invalid option; target has been hit already";
+//     }
+//     // console.log((attempt))
+//     // console.log((ship.coordinates))
+//     else if (ship.coordinates.toString().includes
+//     (attempt.toString())) {
+//       return ship.hitCount++, missed.push(attempt), isSunk(ship), "hit";
+//     } else {
+//       return missed.push(attempt), isSunk(ship), "miss";
+//     }
+//   };
+
+const receiveAttack = (ship, attempt) => {
+    console.log(ship)
+    console.log(attempt)
     
-    const placeShip = (length, shipName, xAxisChoice, yAxisChoice, orientation) => {
-        shipName = ship(shipName, length, 0, false, [], orientation)
-                        
-        // console.log(shipName.length)
-        // console.log(yAxisChoice)
-
-        //checks if ship clips is within the grid
-        console.log(yAxisChoice)
-
-
-        if (orientation === 'vertical') {
-                const yOrigin = yAxis.indexOf(yAxisChoice)
-                let shipYCoordinateArr = (yAxis.slice(yOrigin, yOrigin + shipName.length))
-
-                //checks if ship is out of bounds
-                if (shipName.length + yOrigin > 10)
-                {return}
-
-                // if  (shipName.length + yAxisChoice > 10)
-                // {return console.log('out of grid')}
-
-                //check if coordinates are occupied by another ship
-                for (const element of shipYCoordinateArr) {                    
-                    for (let i = 0; i < shipCoordinateList.length; i++)
-                    {let choice = [xAxisChoice, element]
-                        if(shipCoordinateList[i] == choice.toString())
-                    {return}}
-
-                    shipName.coordinates.push([xAxisChoice, element])
-                    shipCoordinateList.push([xAxisChoice, element])
-                }
-                activeShipList.push(shipName)
-            } else if (orientation === 'horizontal')    {
-                const xOrigin = xAxis.indexOf(xAxisChoice)
-                let shipXCoordinateArr = (xAxis.slice(xOrigin, xOrigin + shipName.length))
-
-                // console.log(shipName.length + xOrigin)
-                // console.log(yAxisChoice)
-
-
-                if (yAxisChoice === 'a' && shipName.length + xOrigin > 10)
-                {return console.log('asdf')}
-
-                //checks if ship clips through grid
-                if (shipName.length + xAxisChoice > 11) 
-                {return}
-
-                //check if ship is out of bounds
-                if (shipName.length + xOrigin > 12)
-                {return}
-
-                
-                //check if coordinates are occupied by another ship
-                for (const element of shipXCoordinateArr) {                   
-                    for (let i = 0; i < shipCoordinateList.length; i++)
-                    {let choice = [element, yAxisChoice]
-                        if(shipCoordinateList[i] == choice.toString())
-                    {return}}
-                    
-                    shipName.coordinates.push([element, yAxisChoice])
-                    shipCoordinateList.push([element, yAxisChoice])                   
-                }
-                activeShipList.push(shipName)
-            }      
-            // console.log(shipCoordinateList)
-        return shipName
+    if (ship === undefined && (missed.toString().includes(attempt.toString())) === false) {        
+        return missed.push(attempt), console.log(missed)}
+    else if (missed.toString().includes(attempt.toString())) {
+        console.log("invalid option; target has been hit already")
+      return;
     }
+    else if (ship.coordinates.toString().includes(attempt.toString())) {
+        console.log(ship.shipName + ' has been hit')
+      return ship.hitCount++, missed.push(attempt), isSunk(ship);
+    }
+  };
+// rewrite code to accomodate hits
 
-return {xAxis, yAxis, missed, receiveAttack, placeShip, activeShipList, gameOverCheck, shipCoordinateList}
-}
+  const placeShip = (
+    length,
+    shipName,
+    xAxisChoice,
+    yAxisChoice,
+    orientation
+  ) => {
+    shipName = ship(shipName, length, 0, false, [], orientation);
 
-const cpuGameboard = gameboard()
-const player1Gameboard = gameboard()
+    // console.log(shipName.length)
+    // console.log(yAxisChoice)
+
+    //checks if ship clips is within the grid
+    // console.log(yAxisChoice);
+
+    if (orientation === "vertical") {
+      const yOrigin = yAxis.indexOf(yAxisChoice);
+      let shipYCoordinateArr = yAxis.slice(yOrigin, yOrigin + shipName.length);
+
+      //checks if ship is out of bounds
+      if (shipName.length + yOrigin > 10) {
+        return;
+      }
+
+      // if  (shipName.length + yAxisChoice > 10)
+      // {return console.log('out of grid')}
+
+      //check if coordinates are occupied by another ship
+      for (const element of shipYCoordinateArr) {
+        for (let i = 0; i < shipCoordinateList.length; i++) {
+          let choice = [xAxisChoice, element];
+          if (shipCoordinateList[i] == choice.toString()) {
+            return;
+          }
+        }
+
+        shipName.coordinates.push([xAxisChoice, element]);
+        shipCoordinateList.push([xAxisChoice, element]);
+      }
+      activeShipList.push(shipName);
+    } else if (orientation === "horizontal") {
+      const xOrigin = xAxis.indexOf(xAxisChoice);
+      let shipXCoordinateArr = xAxis.slice(xOrigin, xOrigin + shipName.length);
+
+      // console.log(shipName.length + xOrigin)
+      // console.log(yAxisChoice)
+
+      if (yAxisChoice === "a" && shipName.length + xOrigin > 10) {
+        return;
+      }
+
+      //checks if ship clips through grid
+      if (shipName.length + xAxisChoice > 11) {
+        return;
+      }
+
+      //check if ship is out of bounds
+      if (shipName.length + xOrigin > 12) {
+        return;
+      }
+
+      //check if coordinates are occupied by another ship
+      for (const element of shipXCoordinateArr) {
+        for (let i = 0; i < shipCoordinateList.length; i++) {
+          let choice = [element, yAxisChoice];
+          if (shipCoordinateList[i] == choice.toString()) {
+            return;
+          }
+        }
+
+        shipName.coordinates.push([element, yAxisChoice]);
+        shipCoordinateList.push([element, yAxisChoice]);
+      }
+      activeShipList.push(shipName);
+    }
+    return shipName
+    // console.log(shipCoordinateList)
+  };
+
+  return {
+    xAxis,
+    yAxis,
+    missed,
+    receiveAttack,
+    placeShip,
+    activeShipList,
+    gameOverCheck,
+    shipCoordinateList,
+  };
+};
+
+const botGameboard = gameboard();
+const player1Gameboard = gameboard();
 
 // const carrier = player1Gameboard.placeShip(5, 'carrier')
 // const battleship = player1Gameboard.placeShip(4, 'battleship')
@@ -128,42 +180,79 @@ const player1Gameboard = gameboard()
 // const destroyer = player1Gameboard.placeShip(2, 'destroyer', 6, 'c', 'horizontal')
 // const ssConflict = player1Gameboard.placeShip(3, 'ssConflict', 3, 'a', 'vertical')
 
-const carrier = {shipObjectLength: 5, shipObjectName: 'carrier'}
-const battleship = {shipObjectLength: 4, shipObjectName: 'battleship'}
-const cruiser = {shipObjectLength: 3, shipObjectName: 'cruiser'}
-const submarine = {shipObjectLength: 3, shipObjectName: 'submarine'}
-const destroyer = {shipObjectLength: 2, shipObjectName: 'destroyer'}
-const ssConflict = (3, 'ssConflict')
+const carrier = { shipObjectLength: 5, shipObjectName: "carrier" };
+const battleship = { shipObjectLength: 4, shipObjectName: "battleship" };
+const cruiser = { shipObjectLength: 3, shipObjectName: "cruiser" };
+const submarine = { shipObjectLength: 3, shipObjectName: "submarine" };
+const destroyer = { shipObjectLength: 2, shipObjectName: "destroyer" };
+const ssConflict = (3, "ssConflict");
 
-
-const ships = [
-    battleship,
-    carrier,
-    cruiser,
-    destroyer,
-    submarine
-]
-
+const ships = [battleship, carrier, cruiser, destroyer, submarine];
 
 const referee = () => {
+  player1Gameboard.gameOverCheck();
+  return player1Gameboard, botGameboard;
+};
 
-    
-    player1Gameboard.gameOverCheck()
-    return player1Gameboard, cpuGameboard
-}
+const botFunction = () => {
 
-referee()
+            const botRandomChoiceX = () => {
+                return Math.floor(Math.random() * 9)
+            }
 
-// player1Gameboard.receiveAttack(cruiser, [6, 'a'])
-// player1Gameboard.receiveAttack(cruiser, [6, 'a'])
-// player1Gameboard.receiveAttack(cruiser, [3, 'c'])
-// player1Gameboard.receiveAttack(cruiser, [3, 'b'])
-// player1Gameboard.receiveAttack(cruiser, [3, 'a'])
+            const botRandomChoiceY = () => {
+                return Math.floor(Math.random() * 9)
+            }
 
-// export {player1Gameboard}
-// export {boat}
-// export {cruiser}
+            const botRandomOrientation = () => {
+                if (Math.floor(Math.random() * 2) === 1) 
+                {return ('horizonatal')}
+                else if (Math.floor(Math.random() * 2) === 0) 
+                {return ('vertical') }
+                else {return 'vertical'}
+            }
 
 
-export {player1Gameboard}
-export {ships}
+
+            // const botChoice = (count) => {
+            //     botGameboard.placeShip(
+            //         ships[count].shipObjectLength,
+            //         ships[count].shipObjectName, 
+            //         (botGameboard.xAxis[count]),
+            //         (botGameboard.yAxis[count]),
+            //         'vertical')
+            // }
+
+            const botChoice = (count) => {
+                botGameboard.placeShip(
+                    ships[count].shipObjectLength,
+                    ships[count].shipObjectName, 
+                    (botGameboard.xAxis[count + 1]),
+                    (botGameboard.yAxis[count]),
+                    'vertical')
+                }
+                
+                
+                
+                const botChoiceFunction = () => {
+                    for (let i = 0; i < 5; i++)    
+                    {botChoice(i)}
+                }
+                botChoiceFunction()
+                
+                // if(botGameboard.activeShipList.length !== 5) {botChoiceFunction()}
+                
+                // console.log(botGameboard.activeShipList)
+                // console.log(botGameboard.shipCoordinateList)
+
+            
+        }
+
+botFunction()
+
+referee();
+
+export { player1Gameboard };
+export { botGameboard };
+export { ships };
+export {referee};
